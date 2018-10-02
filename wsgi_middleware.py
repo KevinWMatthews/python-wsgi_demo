a0f3b886077b57bd44c92380259090772d126a63
Kevin Matthews <kmatthews@cyberdata.net>
2018-10-02 11:55:01 -0700

    Create simple Middleware application

--- wsgi_middleware.py
+++ wsgi_middleware.py
@@ -1,5 +1,8 @@
 import wsgiref.simple_server
 
+# Set to false to send a response from the middleware
+PASS_DOWN_STACK = True
+
 SERVER_HOSTNAME = 'localhost'
 SERVER_PORT = 8051
 
@@ -13,11 +16,42 @@ def simple_app(environ, start_response):
     start_response(status, response_headers)
     return [response_body]
 
+class Middleware():
+    ''' Middleware that passes data down the WSGI stack '''
+
+    # Server interface?
+    def __init__(self, app):
+        ''' "Wrap" the application '''
+        self.wrapped_app = app
+
+    # WSGI application interface:
+    #   a class that contains a __call__() method
+    def __call__(self, environ, start_response):
+        if PASS_DOWN_STACK:
+            # Pass data down the WSGI stack
+            # Let the wrapped app execute the server's callback
+            #
+            # We could edit this response if we wanted to?
+            return self.wrapped_app(environ, start_response)
+        else:
+            # Send a response to the server without passing data to the app below
+            response_body = 'Response from middleware\n'.encode('utf-8')
+            status = '403 Forbidden'
+            response_headers = [
+                ('Content-Type', 'text/plain'),
+                ('Content-Length', str(len(response_body))),
+            ]
+            start_response(status, response_headers)
+            return [response_body]
+
+
 if __name__ == '__main__':
+    middleware = Middleware(simple_app)
+
     httpd = wsgiref.simple_server.make_server(
         SERVER_HOSTNAME,
         SERVER_PORT,
-        simple_app
+        middleware
     )
     print('Listening for a single request at {}:{}'.format(SERVER_HOSTNAME, SERVER_PORT))
     httpd.handle_request()
